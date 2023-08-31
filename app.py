ca7965af51397f5a7f21c1b6b1de0d9e845df47b
Aayush Adhikari <adhikariaayush37@gmail.com>
2023-08-31 15:19:00 +0545

    update

--- app.py
+++ app.py
@@ -3,14 +3,15 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.metrics import accuracy_score
-from model.data_preprocessing import preprocess_data
-from model.model_training import train_model, evaluate_model
+from model.data_preprocessing import preprocess_data  # Update the import based on your folder structure
+from model.model_training import train_model, evaluate_model  # Update the import based on your folder structure
 
 st.title("Sports Game Outcome Prediction")
 st.sidebar.title("Settings")
 
-# Initialize X_train as an empty DataFrame
+# Initialize X_train and y_train as empty DataFrames
 X_train = pd.DataFrame()
+y_train = pd.Series()
 
 uploaded_file = st.sidebar.file_uploader("Upload your own CSV:", type=["csv"])
 
@@ -19,6 +20,12 @@ if uploaded_file is not None:
     X_train, X_test, y_train, y_test = preprocess_data(data)
     model = train_model(X_train, y_train)
     st.write("New model trained with uploaded data!")
+
+    # Plot the distribution of outcomes
+    fig0, ax0 = plt.subplots()
+    sns.countplot(x=y_train, ax=ax0)
+    ax0.set_title('Distribution of Outcomes')
+    st.pyplot(fig0)
 
 # Check if X_train is not empty before proceeding
 if not X_train.empty:
@@ -38,18 +45,20 @@ if not X_train.empty:
     test_accuracy = accuracy_score(y_test, model.predict(X_test))
     st.sidebar.write(f"Test Accuracy: {test_accuracy:.2f}")
 
+    # Plot correlation matrix heatmap
     corr_matrix = X_train.corr()
-    plt.figure(figsize=(10, 8))
-    sns.heatmap(corr_matrix, annot=True, cmap="coolwarm")
-    st.pyplot()
+    fig1, ax1 = plt.subplots(figsize=(10, 8))
+    sns.heatmap(corr_matrix, annot=True, cmap="coolwarm", ax=ax1)
+    st.pyplot(fig1)
 
+    # Plot feature importance
     importance = model.feature_importances_
     feature_names = X_train.columns
-    plt.figure(figsize=(10, 6))
-    sns.barplot(x=importance, y=feature_names)
-    plt.xlabel("Feature Importance")
-    plt.ylabel("Features")
-    plt.title("Feature Importance")
-    st.pyplot()
+    fig2, ax2 = plt.subplots(figsize=(10, 6))
+    sns.barplot(x=importance, y=feature_names, ax=ax2)
+    ax2.set_xlabel("Feature Importance")
+    ax2.set_ylabel("Features")
+    ax2.set_title("Feature Importance")
+    st.pyplot(fig2)
 else:
     st.write("Please upload a CSV file to proceed.")

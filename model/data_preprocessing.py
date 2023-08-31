32f6472cb4832d3cefad497a403927dc4b4b2708
Aayush Adhikari <adhikariaayush37@gmail.com>
2023-08-31 15:00:02 +0545

    update

--- model/data_preprocessing.py
+++ model/data_preprocessing.py
@@ -1,49 +1,23 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler, LabelEncoder
+from scipy import stats
+from imblearn.over_sampling import SMOTE
 
 def preprocess_data(file_path):
-    # Load the dataset from the provided file
     data = pd.read_csv(file_path)
-
-    # Drop rows with missing values
     data = data.dropna()
 
-    reference_date = pd.to_datetime('2023-01-01')  # Replace with an appropriate reference date
-    data['DaysSinceReference'] = (pd.to_datetime(data['Date']) - reference_date).dt.days
+    # Remove outliers using Z-score
+    z_scores = stats.zscore(data.select_dtypes(include=['float64', 'int64']))
+    abs_z_scores = np.abs(z_scores)
+    filtered_entries = (abs_z_scores < 3).all(axis=1)
+    data = data[filtered_entries]
 
-    
+    # ... (rest of the code remains the same)
 
-    # Feature Engineering: Extract year, month, and day of the week from the date
-    data['Year'] = data['Date'].dt.year
-    data['Month'] = data['Date'].dt.month
-    data['DayOfWeek'] = data['Date'].dt.dayofweek
-
-    # Encode categorical variables using Label Encoding
-    label_encoder = LabelEncoder()
-    data['HomeTeam'] = label_encoder.fit_transform(data['HomeTeam'])
-    data['AwayTeam'] = label_encoder.fit_transform(data['AwayTeam'])
-    data['Venue'] = label_encoder.fit_transform(data['Venue'])
-    data['Outcome'] = label_encoder.fit_transform(data['Outcome'])  # Encoding the outcome column
-
-    # Normalize numerical features using StandardScaler
-    scaler = StandardScaler()
-    numerical_cols = ['HomeTotalWins', 'HomeTotalGames', 'HomeTeamPerformance', 'AwayTeamPerformance',
-                      'Feature1', 'Feature2', 'Feature3']
-    data[numerical_cols] = scaler.fit_transform(data[numerical_cols])
-
-    # Feature Engineering: Calculate win rate and performance differential
-    data['WinRate'] = data['HomeTotalWins'] / data['HomeTotalGames']
-    data['PerformanceDiff'] = data['HomeTeamPerformance'] - data['AwayTeamPerformance']
-
-    # One-Hot Encoding for day of the week
-    data = pd.get_dummies(data, columns=['DayOfWeek'], prefix='Day')
-
-    # Split the data into features (X) and target (y)
-    X = data.drop(columns=['Outcome'])
-    y = data['Outcome']
-
-    # Split the data into training and testing sets
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    # Handle imbalanced classes using SMOTE
+    smote = SMOTE(random_state=42)
+    X_train, y_train = smote.fit_resample(X_train, y_train)
 
     return X_train, X_test, y_train, y_test
--- model/model_training.py
+++ model/model_training.py
@@ -1,24 +1,21 @@
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
+from sklearn.model_selection import GridSearchCV
 
 def train_model(X_train, y_train):
-    """
-    Train a RandomForestClassifier model.
+    params = {
+        'n_estimators': [50, 100, 150],
+        'max_depth': [None, 10, 20, 30],
+        'min_samples_split': [2, 5, 10]
+    }
+    model = RandomForestClassifier(random_state=42)
+    grid_search = GridSearchCV(model, params, cv=5)
+    grid_search.fit(X_train, y_train)
+    best_model = grid_search.best_estimator_
 
-    Args:
-        X_train (DataFrame): Features of the training data.
-        y_train (Series): Target labels of the training data.
+    return best_model
 
-    Returns:
-        RandomForestClassifier: Trained model.
-    """
-    # Initialize a RandomForestClassifier model
-    model = RandomForestClassifier(n_estimators=100, random_state=42)
-
-    # Train the model using the training data
-    model.fit(X_train, y_train)
-
-    return model
+# ... (rest of the code remains the same)
 
 def evaluate_model(model, X_test, y_test):
     """
